ff0bb1bbbc3a241d7d8624ab7980c9ed93a0ab99
kisankang <rkdrltks@gmail.com>
2023-04-25 16:15:13 +0800

    resolve typo made while resolving confliction

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mysite/data/service/theme_service.dart';
-]import 'package:mysite/data/service/drawer_service.dart';
+import 'package:mysite/data/service/drawer_service.dart';
 import 'package:mysite/data/service/scroll_service.dart';
 import 'my_site.dart';
 
